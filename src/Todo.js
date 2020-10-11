53415e7c7f32f741679fbf84994cd9bee1454b02
= <nassiromid@gmail.com>
2020-10-11 14:04:06 -0700

    Added modal skeleton

--- src/Todo.js
+++ src/Todo.js
@@ -1,20 +1,55 @@
-import React from 'react'
-import { Button, List, ListItem, ListItemText} from '@material-ui/core';
+import React, { useState } from 'react'
+import { List, Modal, ListItem, ListItemText} from '@material-ui/core';
 import db from './firebase';
 
+import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
+import { makeStyles } from '@material-ui/core/styles';
 
 import './Todo.css'
 
 function Todo(props) {
+
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+          position: 'absolute',
+          width: 400,
+          backgroundColor: theme.palette.background.paper,
+          border: '2px solid #000',
+          boxShadow: theme.shadows[5],
+          padding: theme.spacing(2, 4, 3),
+        },
+      }));
+
+      
+    const [open, setOpen] = useState(false);
+    const classes = useStyles();
+    
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+      
+
     return (
+        <>
         <List className="todo__item">
             <ListItem>
-                <ListItemText primary={props.todo.todo} secondary="todo"></ListItemText>
+                <ListItemText primary="Todo" secondary={props.todo.todo}></ListItemText>
             </ListItem>
-            <DeleteIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()}>Delete Todo</DeleteIcon>
+            <EditIcon onClick={handleOpen} />
+            <DeleteIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()} className="todo__deleteIcon" />
+            <Modal
+                open={open}
+                onClose={e => setOpen(false)}
+            >
+                <div className={classes.paper}>
+                    <h1>Edit Todo</h1>
+                    <button onClick={e => setOpen(false)}>X</button>
+                </div>   
+            </Modal>
         </List>
-
+        </>
     )
 }
 
