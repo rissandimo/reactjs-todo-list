d42fd3ca367b09e30a36c141b827a0ab87cace74
= <nassiromid@gmail.com>
2020-10-10 20:17:09 -0700

    Add todo to array and display in list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import React, { useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = e => {
+    e.preventDefault();
+    console.log('entere clicked');
+    setTodos([...todos, input]);
+    console.log(todos);
+    setInput('');
+  }
   return (
     <div className="app">
-
+        <form>
+          <FormControl>
+            <InputLabel>Enter a todo item</InputLabel>
+            <Input value={input} placeholder="Enter a todo item" onChange={e => setInput(e.target.value)} />
+          </FormControl>
+          <Button variant="contained" type="submit" color="primary" disabled={!input} onClick={addTodo}>Add Todo</Button>
+        </form>
+        <ul>
+        {todos.map(todo => (
+            <li key={todo}>{todo}</li>
+        ))}
+        </ul>
     </div>
   );
 }
